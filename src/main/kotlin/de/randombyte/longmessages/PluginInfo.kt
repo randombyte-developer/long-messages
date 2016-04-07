75391b249b8cfad670124c60da8c6d136fc3408a
randombyte-developer <apps.randombyte@gmail.com>
2016-04-07 20:04:54 +0200

    Bumped version to v1!

--- src/main/kotlin/de/randombyte/longmessages/PluginInfo.kt
+++ src/main/kotlin/de/randombyte/longmessages/PluginInfo.kt
@@ -3,6 +3,6 @@ package de.randombyte.longmessages
 object PluginInfo {
     const val NAME = "LongMessages"
     const val ID = "de.randombyte.longmessages"
-    const val VERSION = "v0.3.3"
+    const val VERSION = "v1.0.0"
     const val AUTHOR = "RandomByte"
 }
